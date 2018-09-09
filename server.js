414ad4557d13828d079fdab29ecd7cf331708d94
Andrei Istoc <andreistoc@yahoo.com>
2018-09-09 12:21:56 +0300

    Added Files

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static(__dirname + '/'))
+
+app.get('/', function(req,res) {
+  res.sendFile(process.cwd() + '/index.html');
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000...')
+});
